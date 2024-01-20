49c2ead1a7f8acf0b429f8bd80a867b3e41520bc
ILIA ILIA(Ilia Litvinenko) <lila96611@gmail.com>
2024-01-20 16:02:21 +0300

    The hook directory has been added and useAuthCheck has been created

---
+++ client/src/hooks/useAuthCheck.js
@@ -0,0 +1,16 @@
+import { useContext, useEffect } from "react"
+import { Context } from "../context"
+import tokenService from "../service/tokenService"
+
+export function useAuthCheck() {
+  const {userStore, alertStore} = useContext(Context)
+  useEffect(() => {
+    if(tokenService.getAccessToken()) {
+      userStore.authCheck().then((res) => {
+        if(res.error) {
+          alertStore.alertOpen(res.error, 'info')
+        }
+      })
+    }
+  }, [])
+}
